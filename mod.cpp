a65a9056571cf3c9f6c41ce085d5c8a29258cdd6
x-00010 <x00010@protonmail.com>
2020-07-05 09:46:20 +0100

    Renamed .paa file

--- mod.cpp
+++ mod.cpp
@@ -1,12 +1,12 @@
 name = "Who Killed Corporal Kerry?";
-picture = "\XIM_CK\XIMCorpKerry.paa";
+picture = "\XIM_CK\XIM_CK.paa";
 actionName = "NYI";
 action = "https://www.tesco.com";
 description = "A mod which turns Arma 3 into an action packed movie!";
-logo = "\XIM_CK\XIMCorpKerry.paa";
-logoOver = "\XIM_CK\XIMCorpKerry.paa";
+logo = "\XIM_CK\XIM_CK.paa";
+logoOver = "\XIM_CK\XIM_CK.paa";
 tooltip = "Who Killed Corporal Kerry?";
 tooltipOwned = "";
 overview = "A mod which turns Arma 3 into an action packed movie!";
 author = "x00010";
-overviewPicture = "\XIM_CK\XIMCorpKerry.paa";
+overviewPicture = "\XIM_CK\XIM_CK.paa";
